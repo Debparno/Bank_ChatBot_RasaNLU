f75ac47a3fc4ac08c813f5216b10af3190ed16f9
Debparno <41444610+Debparno@users.noreply.github.com>
2020-01-11 16:49:43 +0530

    Update registry.py

--- rasa_nlu/registry.py
+++ rasa_nlu/registry.py
@@ -17,26 +17,16 @@ from typing import Optional
 from typing import Text
 from typing import Type
 
-from rasa_nlu.classifiers.keyword_intent_classifier import \
-    KeywordIntentClassifier
-from rasa_nlu.classifiers.mitie_intent_classifier import MitieIntentClassifier
-from rasa_nlu.classifiers.sklearn_intent_classifier import \
-    SklearnIntentClassifier
-from rasa_nlu.extractors.duckling_extractor import DucklingExtractor
-from rasa_nlu.extractors.duckling_http_extractor import DucklingHTTPExtractor
+
+from rasa_nlu.classifiers.sklearn_intent_classifier import SklearnIntentClassifier
 from rasa_nlu.extractors.entity_synonyms import EntitySynonymMapper
-from rasa_nlu.extractors.mitie_entity_extractor import MitieEntityExtractor
 from rasa_nlu.extractors.spacy_entity_extractor import SpacyEntityExtractor
 from rasa_nlu.extractors.crf_entity_extractor import CRFEntityExtractor
-from rasa_nlu.featurizers.mitie_featurizer import MitieFeaturizer
-from rasa_nlu.featurizers.ngram_featurizer import NGramFeaturizer
 from rasa_nlu.featurizers.regex_featurizer import RegexFeaturizer
 from rasa_nlu.featurizers.spacy_featurizer import SpacyFeaturizer
 from rasa_nlu.model import Metadata
-from rasa_nlu.tokenizers.mitie_tokenizer import MitieTokenizer
 from rasa_nlu.tokenizers.spacy_tokenizer import SpacyTokenizer
 from rasa_nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
-from rasa_nlu.utils.mitie_utils import MitieNLP
 from rasa_nlu.utils.spacy_utils import SpacyNLP
 
 if typing.TYPE_CHECKING:
@@ -46,13 +36,12 @@ if typing.TYPE_CHECKING:
 # Classes of all known components. If a new component should be added,
 # its class name should be listed here.
 component_classes = [
-    SpacyNLP, MitieNLP,
-    SpacyEntityExtractor, MitieEntityExtractor, DucklingExtractor,
-    CRFEntityExtractor, DucklingHTTPExtractor,
+    SpacyNLP,
+    SpacyEntityExtractor, CRFEntityExtractor,
     EntitySynonymMapper,
-    SpacyFeaturizer, MitieFeaturizer, NGramFeaturizer, RegexFeaturizer,
-    MitieTokenizer, SpacyTokenizer, WhitespaceTokenizer,
-    SklearnIntentClassifier, MitieIntentClassifier, KeywordIntentClassifier,
+    SpacyFeaturizer, RegexFeaturizer,
+    SpacyTokenizer, WhitespaceTokenizer,
+    SklearnIntentClassifier,
 ]
 
 # Mapping from a components name to its class to allow name based lookup.
@@ -71,49 +60,21 @@ registered_pipeline_templates = {
         "ner_synonyms",
         "intent_classifier_sklearn",
     ],
-    "mitie": [
-        "nlp_mitie",
-        "tokenizer_mitie",
-        "ner_mitie",
-        "ner_synonyms",
-        "intent_entity_featurizer_regex",
-        "intent_classifier_mitie",
-    ],
-    "mitie_sklearn": [
-        "nlp_mitie",
-        "tokenizer_mitie",
-        "ner_mitie",
-        "ner_synonyms",
-        "intent_entity_featurizer_regex",
-        "intent_featurizer_mitie",
-        "intent_classifier_sklearn",
-    ],
-    "keyword": [
-        "intent_classifier_keyword",
-    ],
+   
     # this template really is just for testing
     # every component should be in here so train-persist-load-use cycle can be
     # tested they still need to be in a useful order - hence we can not simply
     # generate this automatically.
     "all_components": [
         "nlp_spacy",
-        "nlp_mitie",
         "tokenizer_whitespace",
-        "tokenizer_mitie",
         "tokenizer_spacy",
-        "intent_featurizer_mitie",
         "intent_featurizer_spacy",
-        "intent_featurizer_ngrams",
         "intent_entity_featurizer_regex",
-        "ner_mitie",
         "ner_crf",
         "ner_spacy",
-        "ner_duckling",
-        "ner_duckling_http",
         "ner_synonyms",
-        "intent_classifier_keyword",
         "intent_classifier_sklearn",
-        "intent_classifier_mitie",
     ]
 }
 
